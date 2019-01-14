3f3c1bda9ad5b6ae8caddbdf72ae8a08e326a8e5
stephanieli333 <46694132+stephanieli333@users.noreply.github.com>
2019-01-14 18:02:22 -0500

    Update DecisionTree.java

--- DecisionTree.java
+++ DecisionTree.java
@@ -6,7 +6,7 @@ import java.lang.Math;
 public class DecisionTree implements Serializable {
 
 	DTNode rootDTNode;
-	int minSizeDatalist; //minimum number of datapoints that should be present in the dataset so as to initiate a split
+	int minSizeDatalist; //minimum number of datapoints that should be present in the dataset to initiate a split
 	//Mention the serialVersionUID explicitly in order to avoid getting errors while deserializing.
 	public static final long serialVersionUID = 343L;
 	public DecisionTree(ArrayList<Datum> datalist , int min) {
@@ -22,8 +22,6 @@ public class DecisionTree implements Serializable {
 		int attribute; // only defined if node is not a leaf
 		double threshold;  // only defined if node is not a leaf
 
-
-
 		DTNode left, right; //the left and right child of a particular node. (null if leaf)
 
 		DTNode() {
@@ -31,14 +29,11 @@ public class DecisionTree implements Serializable {
 			threshold = Double.MAX_VALUE;
 		}
 
-
-
 		// this method takes in a datalist (ArrayList of type datum) and a minSizeInClassification (int) and returns
 		// the calling DTNode object as the root of a decision tree trained using the datapoints present in the
 		// datalist variable
-		// Also, KEEP IN MIND that the left and right child of the node correspond to "less than" and "greater than or equal to" threshold
+		// The left and right child of the node correspond to "less than" and "greater than or equal to" threshold
 		DTNode fillDTNode(ArrayList<Datum> datalist) {
-			//YOUR CODE HERE
 			double entropy = calcEntropy(datalist);
 			double avgEntropy;
 			ArrayList<Datum> list1a = new ArrayList<Datum>();
@@ -105,21 +100,21 @@ public class DecisionTree implements Serializable {
 			return this;
 		}
 
-		//This is a helper method. Given a datalist, this method returns the label that has the most
-		// occurences. In case of a tie it returns the label with the smallest value (numerically) involved in the tie.
+		// Given a datalist, this method returns the label that has the most occurences
+		// In case of a tie it returns the label with the smallest value in the tie
 		int findMajority(ArrayList<Datum> datalist)
 		{
 			int l = datalist.get(0).x.length;
 			int [] votes = new int[l];
 
-			//loop through the data and count the occurrences of datapoints of each label
+			//loops through the data and count the occurrences of datapoints of each label
 			for (Datum data : datalist)
 			{
 				votes[data.y]+=1;
 			}
 			int max = -1;
 			int max_index = -1;
-			//find the label with the max occurrences
+			// finds the label with the max occurrences
 			for (int i = 0 ; i < l ;i++)
 			{
 				if (max<votes[i])
@@ -131,10 +126,9 @@ public class DecisionTree implements Serializable {
 			return max_index;
 		}
 
-		// This method takes in a datapoint (excluding the label) in the form of an array of type double (Datum.x) and
+		// takes in a datapoint (excluding the label) in the form of an array of type double (Datum.x) and
 		// returns its corresponding label, as determined by the decision tree
 		int classifyAtNode(double[] xQuery) {
-			//YOUR CODE HERE
 			if(this!=null && this.leaf) {
 				return this.label;
 			}
@@ -161,7 +155,6 @@ public class DecisionTree implements Serializable {
 		//at DTNode object passed as the parameter
 		public boolean equals(Object dt2)
 		{
-			//YOUR CODE HERE
 			// implement a traversal and check to see if all nodes are equal
 			// ie for internal nodes, attributes and thresholds should be the same
 			// for leaf nodes, the labels should be the same
@@ -227,30 +220,7 @@ public class DecisionTree implements Serializable {
 		DTNode node = this.rootDTNode;
 		return node.classifyAtNode( xQuery );
 	}
-
-    // Checks the performance of a DecisionTree on a dataset
-    //  This method is provided in case you would like to compare your
-    //results with the reference values provided in the PDF in the Data
-    //section of the PDF
-
-    String checkPerformance( ArrayList<Datum> datalist)
-	{
-		DecimalFormat df = new DecimalFormat("0.000");
-		float total = datalist.size();
-		float count = 0;
-
-		for (int s = 0 ; s < datalist.size() ; s++) {
-			double[] x = datalist.get(s).x;
-			int result = datalist.get(s).y;
-			if (classify(x) != result) {
-				count = count + 1;
-			}
-		}
-
-		return df.format((count/total));
-	}
-
-
+	
 	//Given two DecisionTree objects, this method checks if both the trees are equal by
 	//calling onto the DTNode.equals() method
 	public static boolean equals(DecisionTree dt1,  DecisionTree dt2)
